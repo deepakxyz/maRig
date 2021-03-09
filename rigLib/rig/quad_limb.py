802cac373d34e76f063f0895fe79da94258e8fe4
Deepak <deepakrajan.grea@gmail.com>
2021-03-09 14:12:04 +0530

    arm.nxt

--- rigLib/rig/quad_limb.py
+++ rigLib/rig/quad_limb.py
@@ -82,3 +82,54 @@ def placeholder_loc(joint,loc,ctrlType,ctrlColor,ctrlShape,aimAxis):
     mc.move(location[0],location[1],location[2],sub_loc,wd=True)
     mc.parent(sub_loc,main_loc)
     temp_constraint = mc.parentConstraint(joint,main_loc)
+
+
+def ik_fk_blender_set(constraint_list, joints, name, suffix, joint, loc, type, scale, color, attribName, fk_grps,
+                      ik_grps):
+    # switch node
+    switch = placeHolder(name=name, suffix=suffix, joint=joint, loc=loc, type=type, aimAxis="Y", scale=scale,
+                         color=color, parent="", constraintType="point")
+
+    # create a IK_FK_blend attribute
+    mc.addAttr(switch[1], keyable=True, longName=attribName, attributeType="double", min=0, max=1, dv=0)
+
+    # create reverse node
+    reverse_node = mc.createNode("reverse")
+
+    # connect Reverse
+    mc.connectAttr(switch[1] + "." + attribName, reverse_node + ".input.inputX")
+
+    # IK FK Switch connection
+    i = 0
+    for constraint in constraint_list:
+        mc.connectAttr(switch[1] + "." + attribName, constraint + "." + joints[i] + "_FKW1")
+        # print(switch[1] + "." + attribName, constraint + "."+ joints[i]+ "_FKW1")
+        mc.connectAttr(reverse_node + ".output.outputX", constraint + "." + joints[i] + "_IKW0")
+        # print(switch[1]+".IK_FK_Switch",constraint + "."+ joints[i]+ "_FKW1")
+        i = i + 1
+
+    # IK FK Control Visiblity connection
+    # FK visibility
+    for fk_offset_grp in fk_grps[0]:
+        mc.connectAttr(switch[1] + "." + attribName, fk_offset_grp + '.' + "visibility")
+        # Connected l_hand_switch_ctrl.IK_FK_Switch to l_humerus_FK_offset_grp.visibility. //
+
+    # IK visibility
+    ik_offset_grp = ik_grps[0][0]
+    pv_offset_grp = ik_grps[1][0]
+    mc.connectAttr(reverse_node + ".output.outputX", ik_offset_grp + '.' + "visibility")
+    mc.connectAttr(reverse_node + ".output.outputX", pv_offset_grp + "." + "visibility")
+
+
+# data
+constraint_list = [u'l_humerus_IF_FK_constraint', u'l_radius_IF_FK_constraint', u'l_wrist_IF_FK_constraint']
+joints = ['l_humerus', 'l_radius', 'l_wrist']
+FK_ctrls = [[u'l_humerus_FK_offset_grp', u'l_radius_FK_offset_grp', u'l_wrist_FK_offset_grp'],
+            [[u'l_humerus_FK_ctrl'], [u'l_radius_FK_ctrl'], [u'l_wrist_FK_ctrl']]]
+IK_grps = [[u'l_wrist_IK_offset_grp', u'l_wrist_IK_ctrl'], [u'l_radius_pv_offset_grp', u'l_radius_pv_ctrl']]
+
+ik_fk_blender_set(constraint_list=constraint_list, joints=joints, name="l_hand_switch", suffix="_ctrl", joint="l_wrist",
+                  loc=[-0.104, 0, 0.108], type="Sphere", scale=0.01, color=24, attribName="IK_FK_Switch",
+                  fk_grps=FK_ctrls, ik_grps=IK_grps)
+
+
